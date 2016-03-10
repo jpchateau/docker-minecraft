c423fb755516037131ab92582714f0e7cc54a6c9
jpchateau <contact@jpchateau.com>
2016-03-10 02:05:31 +0100

    create configuration files

--- Resources/start.sh
+++ Resources/start.sh
@@ -5,5 +5,14 @@ then
     echo "eula=true" > /data/eula.txt
 fi
 
+files=("banned-ips" "banned-players" "ops" "usercache" "whitelist")
+for file in ${files[*]};
+do
+	if [ ! -f /data/$file.json ]
+	then
+		echo "[]" > /data/$file.json
+	fi
+done
+
 cd /data
 java -Xmx1024M -Xms1024M -jar /minecraft_server.jar nogui
